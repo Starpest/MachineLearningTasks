17245d6c438fe66561892dd029008aa12003c8e4
Raghib Mahatab <98721756+Starpest@users.noreply.github.com>
2024-10-01 12:08:13 -0400

    Update patientrecords.py
    added notes

--- patientrecords.py
+++ patientrecords.py
@@ -21,6 +21,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_
 model = DecisionTreeRegressor(random_state=42)
 model.fit(X_train, y_train)
 
+#model evaluation is more accurate when pulling from a large dataset i.e. tensorflow
 # Step 5: Model Evaluation
 train_predictions = model.predict(X_train)
 test_predictions = model.predict(X_test)
